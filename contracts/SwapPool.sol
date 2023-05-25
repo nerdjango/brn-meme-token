b281bba0c51df8b3a37202fccfac0a78d2f055d7
Ifeanyichukwu Welechi <welechi@icloud.com>
2023-05-25 18:54:34 +0100

    Create SwapPool.sol

---
+++ contracts/SwapPool.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract SwapPool is Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable DRA;
+    IERC20 public immutable BRN =
+        IERC20(0x926ecC7687fCFB296E97a2b4501F41A6f5F8C214);
+
+    address public constant deadAddress = address(0xdEaD);
+
+    uint256 public tokensExchanged;
+
+    constructor(address _dra) {
+        DRA = IERC20(_dra);
+    }
+
+    function getDracarys(uint256 _amount) external {
+        require(_amount > 0, "SwapPool: amount must be greater than 0");
+        require(
+            _amount <= BRN.balanceOf(msg.sender),
+            "SwapPool: Insufficient BRN balance"
+        );
+        require(
+            _amount <= BRN.allowance(msg.sender, address(this)),
+            "SwapPool: Insufficient BRN balance"
+        );
+        require(
+            _amount <= DRA.balanceOf(address(this)),
+            "SwapPool: Insufficient DRA balance in pool"
+        );
+        BRN.safeTransferFrom(msg.sender, deadAddress, _amount);
+        DRA.safeTransfer(msg.sender, _amount);
+        tokensExchanged += _amount;
+    }
+
+    function withdraw(uint256 _amount) external onlyOwner {
+        require(
+            _amount <= DRA.balanceOf(address(this)),
+            "SwapPool: Insufficient DRA balance in pool"
+        );
+        DRA.safeTransfer(msg.sender, _amount);
+    }
+
+    function getBurned() external view returns (uint256) {
+        return BRN.balanceOf(deadAddress);
+    }
+}
