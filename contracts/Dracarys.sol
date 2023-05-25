9e5fb9ef3c7b2886f311f64c1d4e56344d4c2fd4
Ifeanyichukwu Welechi <welechi@icloud.com>
2023-05-25 16:47:57 +0100

    token done

---
+++ contracts/Dracarys.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "../node_modules/@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "../node_modules/@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Dracarys is ERC20, Ownable, ReentrancyGuard {
+    using SafeMath for uint256;
+
+    IUniswapV2Router02 public immutable uniswapV2Router;
+    address public immutable uniswapV2Pair;
+
+    event ExcludeFromFees(address indexed account, bool isExcluded);
+
+    event SetAutomatedMarketMakerPair(address indexed pair, bool indexed value);
+
+    constructor() ERC20("Dracarys", "DRA") {
+        uniswapV2Router = IUniswapV2Router02(
+            0x10ED43C718714eb63d5aA57B78B54704E256024E
+        );
+
+        uniswapV2Pair = IUniswapV2Factory(uniswapV2Router.factory()).createPair(
+            address(this),
+            uniswapV2Router.WETH()
+        );
+
+        uint256 totalSupply = 320 * 1e6 * 1e18;
+
+        _mint(msg.sender, totalSupply);
+    }
+
+    receive() external payable {}
+
+    function addLiquidity(
+        uint256 tokenAmount,
+        uint256 ethAmount
+    ) external onlyOwner {
+        // approve token transfer to cover all possible scenarios
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+
+        // add the liquidity
+        uniswapV2Router.addLiquidityETH{value: ethAmount}(
+            address(this),
+            tokenAmount,
+            0, // slippage is unavoidable
+            0, // slippage is unavoidable
+            owner(),
+            block.timestamp
+        );
+    }
+
+    function withdraw(
+        bool tokens
+    ) public onlyOwner nonReentrant returns (bool success) {
+        if (tokens) {
+            uint256 tokenBalance = balanceOf(address(this));
+            require(tokenBalance > 0, "Dracarys: Insufficient Token Balance");
+            _transfer(address(this), msg.sender, tokenBalance);
+        } else {
+            uint256 ethBalance = address(this).balance;
+            require(ethBalance > 0, "Dracarys: Insufficient Ether Balance");
+            payable(msg.sender).transfer(ethBalance);
+        }
+        return true;
+    }
+}
